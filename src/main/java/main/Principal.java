c155812afc4a84ea007037f5147a65830fb7153d
KevinNuness <knfurlanetto@gmail.com>
2024-05-14 09:56:53 -0300

    Fix: Correção de problema com design das interfaces

---
+++ src/main/java/main/Principal.java
@@ -0,0 +1,14 @@
+package main;
+
+import visao.FrmMenuPrincipal;
+
+public class Principal {
+    
+    public static void main (String [] args){
+        
+        FrmMenuPrincipal menu = new FrmMenuPrincipal();
+        
+        menu.setVisible (true);
+        
+    }   
+}
