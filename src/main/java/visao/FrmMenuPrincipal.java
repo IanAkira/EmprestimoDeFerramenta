56e9df33b21a4fcfd843caf47fd0e3d5fcb99ef7
GarraOne <garrra.one@gmail.com>
2024-05-14 10:47:57 -0300

    feat: Implementação das funcionalidades do menu principal.

--- src/main/java/visao/FrmMenuPrincipal.java
+++ src/main/java/visao/FrmMenuPrincipal.java
@@ -26,7 +26,77 @@ public class FrmMenuPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jCadastrarFerramenta = new javax.swing.JMenu();
+        jMenuCadastroAmigo = new javax.swing.JMenuItem();
+        jMenuCadastroFerramenta = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jMenuGerenciarAmigos = new javax.swing.JMenuItem();
+        jMenuGerenciarFerramentas = new javax.swing.JMenuItem();
+        jMenu4 = new javax.swing.JMenu();
+
+        jMenuItem5.setText("jMenuItem5");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jCadastrarFerramenta.setText("Cadastrar");
+
+        jMenuCadastroAmigo.setText("Cadastrar Amigo");
+        jMenuCadastroAmigo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuCadastroAmigoActionPerformed(evt);
+            }
+        });
+        jCadastrarFerramenta.add(jMenuCadastroAmigo);
+
+        jMenuCadastroFerramenta.setText("Cadastrar Ferramenta");
+        jMenuCadastroFerramenta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuCadastroFerramentaActionPerformed(evt);
+            }
+        });
+        jCadastrarFerramenta.add(jMenuCadastroFerramenta);
+
+        jMenuBar1.add(jCadastrarFerramenta);
+
+        jMenu2.setText("Movimento");
+
+        jMenuItem3.setText("Empréstimo");
+        jMenu2.add(jMenuItem3);
+
+        jMenuItem2.setText("Devolução");
+        jMenu2.add(jMenuItem2);
+
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Relatório");
+
+        jMenuGerenciarAmigos.setText("Gerenciar Amigos");
+        jMenuGerenciarAmigos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuGerenciarAmigosActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jMenuGerenciarAmigos);
+
+        jMenuGerenciarFerramentas.setText("Gerenciar Ferramentas");
+        jMenuGerenciarFerramentas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuGerenciarFerramentasActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jMenuGerenciarFerramentas);
+
+        jMenuBar1.add(jMenu3);
+
+        jMenu4.setText("Sobre");
+        jMenuBar1.add(jMenu4);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -36,11 +106,35 @@ public class FrmMenuPrincipal extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 500, Short.MAX_VALUE)
+            .addGap(0, 478, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuCadastroAmigoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuCadastroAmigoActionPerformed
+    FrmCadastrarAmigo amigo = new FrmCadastrarAmigo();
+        
+        amigo.setVisible (true);
+    }//GEN-LAST:event_jMenuCadastroAmigoActionPerformed
+
+    private void jMenuGerenciarAmigosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuGerenciarAmigosActionPerformed
+    FrmGerenciadorAmigo amigo = new FrmGerenciadorAmigo();
+        
+        amigo.setVisible (true);
+    }//GEN-LAST:event_jMenuGerenciarAmigosActionPerformed
+
+    private void jMenuCadastroFerramentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuCadastroFerramentaActionPerformed
+    FrmCadastrarFerramenta ferramenta = new FrmCadastrarFerramenta();
+        
+        ferramenta.setVisible (true);
+    }//GEN-LAST:event_jMenuCadastroFerramentaActionPerformed
+
+    private void jMenuGerenciarFerramentasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuGerenciarFerramentasActionPerformed
+    FrmGerenciadorFerramenta ferramenta = new FrmGerenciadorFerramenta();
+        
+        ferramenta.setVisible (true);
+    }//GEN-LAST:event_jMenuGerenciarFerramentasActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +172,17 @@ public class FrmMenuPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jCadastrarFerramenta;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuCadastroAmigo;
+    private javax.swing.JMenuItem jMenuCadastroFerramenta;
+    private javax.swing.JMenuItem jMenuGerenciarAmigos;
+    private javax.swing.JMenuItem jMenuGerenciarFerramentas;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem5;
     // End of variables declaration//GEN-END:variables
 }
