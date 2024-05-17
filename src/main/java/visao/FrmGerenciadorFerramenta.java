d3a1275f54694b5dce1e305b503aec473fa301fc
HenriqueBent0 <apenasumlucian@hotmail.com>
2024-05-16 23:16:44 -0300

    feat: Completei a tela "GerenciadorFerramentas" e adicionei as suas funcionalidades

--- src/main/java/visao/FrmGerenciadorFerramenta.java
+++ src/main/java/visao/FrmGerenciadorFerramenta.java
@@ -1,6 +1,7 @@
 package visao;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import modelo.Ferramenta;
 
@@ -32,9 +33,12 @@ private Ferramenta objetoFerramenta;
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
+        JTFValor = new javax.swing.JTextField();
+        JTFNome = new javax.swing.JTextField();
+        JTFMarca = new javax.swing.JTextField();
+        JTFApagar = new javax.swing.JButton();
+        JTFEditar = new javax.swing.JButton();
+        JTFCancelar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -49,6 +53,11 @@ private Ferramenta objetoFerramenta;
                 "ID", "Nome", "Marca", "Valor"
             }
         ));
+        JTableFerramenta.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                JTableFerramentaMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(JTableFerramenta);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
@@ -60,21 +69,42 @@ private Ferramenta objetoFerramenta;
 
         jLabel4.setText("Valor:");
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        JTFValor.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                JTFValorActionPerformed(evt);
             }
         });
 
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+        JTFNome.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
+                JTFNomeActionPerformed(evt);
             }
         });
 
-        jTextField3.addActionListener(new java.awt.event.ActionListener() {
+        JTFMarca.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField3ActionPerformed(evt);
+                JTFMarcaActionPerformed(evt);
+            }
+        });
+
+        JTFApagar.setText("Apagar");
+        JTFApagar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JTFApagarActionPerformed(evt);
+            }
+        });
+
+        JTFEditar.setText("Editar");
+        JTFEditar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JTFEditarActionPerformed(evt);
+            }
+        });
+
+        JTFCancelar.setText("Cancelar");
+        JTFCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JTFCancelarActionPerformed(evt);
             }
         });
 
@@ -87,37 +117,50 @@ private Ferramenta objetoFerramenta;
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel1)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(11, 11, 11)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(57, 57, 57)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel3)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(56, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(40, 40, 40)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(JTFValor, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel3)
+                                    .addComponent(JTFNome, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(JTFMarca, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(JTFCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(JTFApagar, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(JTFEditar, javax.swing.GroupLayout.DEFAULT_SIZE, 84, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(47, 47, 47)
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(JTFNome, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(34, 34, 34)
                         .addComponent(jLabel3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(JTFMarca, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(40, 40, 40)
                         .addComponent(jLabel4)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(JTFValor, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(JTFEditar)
+                            .addComponent(JTFApagar)
+                            .addComponent(JTFCancelar)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(28, 28, 28)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 355, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -127,17 +170,121 @@ private Ferramenta objetoFerramenta;
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void JTFValorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFValorActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_JTFValorActionPerformed
 
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+    private void JTFNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFNomeActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField2ActionPerformed
+    }//GEN-LAST:event_JTFNomeActionPerformed
 
-    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
+    private void JTFMarcaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFMarcaActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField3ActionPerformed
+    }//GEN-LAST:event_JTFMarcaActionPerformed
+
+    private void JTFApagarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFApagarActionPerformed
+        try {
+
+            int id = 0;
+            String nome = "";
+            String marca = "";
+            int valor = 0;
+
+            if (this.JTableFerramenta.getSelectedRow() == -1) {
+                throw new Mensagem("Selecione uma ferramenta para apagar primeiro");
+            } else {
+                id = Integer.parseInt(this.JTableFerramenta.getValueAt(this.JTableFerramenta.getSelectedRow(), 0).toString());
+            }
+
+            int respostaUsuario = JOptionPane.showConfirmDialog(null, "Tem certeza que deseja apagar este Ferramenta?");
+
+            if (respostaUsuario == 0) {
+
+                if (this.objetoFerramenta.deleteFerramentaBD(id)) {
+
+                    this.JTFNome.setText("");
+                    this.JTFMarca.setText("");
+                    this.JTFValor.setText("");
+                    JOptionPane.showMessageDialog(rootPane, "Ferramenta Apagada com Sucesso.");
+                }
+            }
+
+            System.out.println(this.objetoFerramenta.getListaFerramenta().toString());
+        } catch (Mensagem erro) {
+            JOptionPane.showMessageDialog(null, erro.getMessage());
+        } finally {
+
+            carregaTabela();
+        }
+    }//GEN-LAST:event_JTFApagarActionPerformed
+
+    private void JTFCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFCancelarActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_JTFCancelarActionPerformed
+
+    private void JTableFerramentaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JTableFerramentaMouseClicked
+        if (this.JTableFerramenta.getSelectedRow() != -1) {
+            String nome = this.JTableFerramenta.getValueAt(this.JTableFerramenta.getSelectedRow(), 1).toString();
+            String marca = this.JTableFerramenta.getValueAt(this.JTableFerramenta.getSelectedRow(), 2).toString();
+            String valor = this.JTableFerramenta.getValueAt(this.JTableFerramenta.getSelectedRow(), 3).toString();
+            
+            this.JTFNome.setText(nome);
+            this.JTFMarca.setText(marca);
+            this.JTFValor.setText(valor);
+        }
+    }//GEN-LAST:event_JTableFerramentaMouseClicked
+
+    private void JTFEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFEditarActionPerformed
+        try {
+
+            int id = 0;
+            String nome = "";
+            String marca = "";
+            int valor = 0;
+         
+            if (this.JTFNome.getText().length() < 1) {
+                throw new Mensagem("Nome deve conter ao menos 1 caracteres.");
+            } else {
+                nome = this.JTFNome.getText();
+            }
+
+
+            if (this.JTFMarca.getText().length() < 1) {
+                throw new Mensagem("A marca deve conter ao menos 1 caracteres.");
+            } else {
+                marca = (this.JTFMarca.getText());
+            }
+            if (this.JTFValor.getText().length() < 1) {
+                throw new Mensagem("O valor deve conter ao menos 1 número.");
+            } else {
+                valor = Integer.parseInt(this.JTFValor.getText());
+            }
+
+            if (this.JTableFerramenta.getSelectedRow() == -1) {
+                throw new Mensagem("Escolha uma Ferramenta para Editar Primeiro");
+            } else {
+                id = Integer.parseInt(this.JTableFerramenta.getValueAt(this.JTableFerramenta.getSelectedRow(), 0).toString());
+            }
+
+            if (this.objetoFerramenta.updateFerramentaBD(id, nome, marca, valor)) {
+                
+                this.JTFNome.setText("");
+                this.JTFMarca.setText("");
+                this.JTFValor.setText("");
+                
+
+                JOptionPane.showMessageDialog(null, "Ferramenta Editado com sucesso.");
+
+            }
+            System.out.println(this.objetoFerramenta.getListaFerramenta().toString());
+        } catch (Mensagem erro) {
+            JOptionPane.showMessageDialog(null, erro.getMessage());
+        } catch (NumberFormatException erro2) {
+            JOptionPane.showMessageDialog(null, "Informe um número válido.");
+        } finally {
+            carregaTabela();
+        }
+    }//GEN-LAST:event_JTFEditarActionPerformed
 
 public void carregaTabela() {
     DefaultTableModel modelo = (DefaultTableModel) this.JTableFerramenta.getModel();
@@ -154,14 +301,17 @@ public void carregaTabela() {
 }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton JTFApagar;
+    private javax.swing.JButton JTFCancelar;
+    private javax.swing.JButton JTFEditar;
+    private javax.swing.JTextField JTFMarca;
+    private javax.swing.JTextField JTFNome;
+    private javax.swing.JTextField JTFValor;
     private javax.swing.JTable JTableFerramenta;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 }
