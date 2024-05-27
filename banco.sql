43ef6a9b5aaaa1bc7f0ebe22d0e4b402003669b0
VictorLSRodrigues <162334303+VictorLSRodrigues@users.noreply.github.com>
2024-05-27 10:15:53 -0300

    Feat: Adicionei a tabela ferramentas

--- banco.sql
+++ banco.sql
@@ -1,4 +1,6 @@
+##cria Banco de dados emprestimodeferramentas
 CREATE DATABASE IF NOT EXISTS emprestimodeferramentas;
+##cria tb_amigos
 USE emprestimodeferramentas;
 DROP TABLE IF EXISTS tb_amigos;
 CREATE TABLE tb_amigos (
@@ -6,4 +8,14 @@ id INTEGER NOT NULL,
 nome VARCHAR(100),
 telefone INTEGER,
 PRIMARY KEY(id)
+);
+##cria tb_ferramentas
+use emprestimodeferramentas;
+DROP TABLE IF EXISTS tb_ferramentas;
+create table tb_ferramentas (
+id integer not null,
+nome varchar(100),
+marca varchar(100),
+valor integer,
+primary key(id)
 );
