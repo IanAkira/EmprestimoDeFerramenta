5bf3d2ed26e2963d0981b2d582f8a027e124ee08
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-05-28 11:44:56 -0300

    Feat: Adicionei a tabela de emprestimos e a tabela de devolucao

--- banco.sql
+++ banco.sql
@@ -9,6 +9,9 @@ nome VARCHAR(100),
 telefone INTEGER,
 PRIMARY KEY(id)
 );
+
+
+
 ##cria tb_ferramentas
 use emprestimodeferramentas;
 DROP TABLE IF EXISTS tb_ferramentas;
@@ -17,5 +20,33 @@ id integer not null,
 nome varchar(100),
 marca varchar(100),
 valor integer,
+primary key(id)
+);
+
+
+
+##cria tb_Emprestimos
+use emprestimodeferramentas;
+DROP TABLE IF EXISTS tb_Emprestimos;
+create table tb_Emprestimos (
+id integer not null,
+nomeAmigo varchar(100),
+NomeDaFerramenta varchar(100),
+data DATE,
+idFerramenta integer,
+primary key(id)
+);
+
+
+
+##cria tb_Devolucao
+use emprestimodeferramentas;
+DROP TABLE IF EXISTS tb_Devolucao;
+create table tb_Devolucao (
+id integer not null,
+nomeAmigo varchar(100),
+NomeDaFerramenta varchar(100),
+data DATE,
+idFerramenta integer,
 primary key(id)
 );
