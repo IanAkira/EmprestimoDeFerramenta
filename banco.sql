1a75ec16ba6f4fcb6a6bfe4765d30eef374d98c1
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-03 09:30:04 -0300

    chore: formatação do banco de dados

--- banco.sql
+++ banco.sql
@@ -1,52 +1,66 @@
 ##cria Banco de dados emprestimodeferramentas
 CREATE DATABASE IF NOT EXISTS emprestimodeferramentas;
-##cria tb_amigos
+
+##utiliza o Banco de dados emprestimodeferramentas
 USE emprestimodeferramentas;
+
+##exclui tb_amigos
 DROP TABLE IF EXISTS tb_amigos;
+
+##cria tb_amigos
 CREATE TABLE tb_amigos (
-id INTEGER NOT NULL,
-nome VARCHAR(100),
-telefone INTEGER,
-PRIMARY KEY(id)
+    id INTEGER NOT NULL,
+    nome VARCHAR(100),
+    telefone INTEGER,
+    PRIMARY KEY(id)
 );
 
 
+##utiliza o Banco de dados emprestimodeferramentas
+use emprestimodeferramentas;
+
+##exclui tb_ferramentas
+DROP TABLE IF EXISTS tb_ferramentas;
 
 ##cria tb_ferramentas
-use emprestimodeferramentas;
-DROP TABLE IF EXISTS tb_ferramentas;
 create table tb_ferramentas (
-id integer not null,
-nome varchar(100),
-marca varchar(100),
-valor integer,
-primary key(id)
+    id integer not null,
+    nome varchar(100),
+    marca varchar(100),
+    valor integer,
+    primary key(id)
 );
 
 
+##utiliza o Banco de dados emprestimodeferramentas
+use emprestimodeferramentas;
+
+##exclui tb_emprestimos
+DROP TABLE IF EXISTS tb_emprestimos;
 
 ##cria tb_emprestimos
-use emprestimodeferramentas;
-DROP TABLE IF EXISTS tb_emprestimos;
 create table tb_emprestimos (
-id integer not null,
-nome varchar(100),
-nomeDaFerramenta varchar(100),
-data varchar(100),
-idFerramenta integer,
-primary key(id)
+    id integer not null,
+    nome varchar(100),
+    nomeDaFerramenta varchar(100),
+    data varchar(100),
+    idFerramenta integer,
+    primary key(id)
 );
 
 
+##utiliza o Banco de dados emprestimodeferramentas
+use emprestimodeferramentas;
+
+##exclui tb_devolucao
+DROP TABLE IF EXISTS tb_devolucao;
 
 ##cria tb_devolucao
-use emprestimodeferramentas;
-DROP TABLE IF EXISTS tb_devolucao;
-create table tb_devolucao (
-id integer not null,
-nomeAmigo varchar(100),
-nomeDaFerramenta varchar(100),
-data varchar(100),
-idFerramenta integer,
-primary key(id)
+    create table tb_devolucao (
+    id integer not null,
+    nomeAmigo varchar(100),
+    nomeDaFerramenta varchar(100),
+    data varchar(100),
+    idFerramenta integer,
+    primary key(id)
 );
